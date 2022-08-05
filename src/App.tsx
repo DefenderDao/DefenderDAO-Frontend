109a7e084b2454ae7b0f5291891d099bebc9fb34
TechnoGecko <cr5697@gmail.com>
2022-08-05 01:53:06 -0700

    Playing with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { createStyles, MantineProvider } from '@mantine/core';
 import Dashboard from './pages/dashboard/Dashboard';
 
-
 function App() {
   return (
-    <MantineProvider theme={{
-      colors: {
-        primaryColor: ['#1f2345', '#283071' ],
-        platinum: ['#eaeaea',]
-      }
-    }}>
-    <div className="App">
-      <Dashboard />
-    </div>
+    <MantineProvider
+      theme={{
+        colors: {
+          primaryColor: ['#1f2345', '#283071'],
+          platinum: ['#eaeaea'],
+          lavender: ['#cbc5ea', '#a99ce9'],
+          orangeRed: ['#FE5F55'],
+          wintergreen: ['#638475'],
+          lightPink: ['#dfb9e9', '#dba4eb']
+        }
+      }}
+    >
+      <div className="App">
+        <Dashboard />
+      </div>
     </MantineProvider>
   );
 }
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,27 +1,44 @@
-import { createStyles } from '@mantine/core'
-import React from 'react'
+import { Button, createStyles } from '@mantine/core';
+import React from 'react';
 
-type Props = {}
+type Props = {};
 
 const useStyles = createStyles((theme, _params, getRef) => ({
-    wrapper: {
-        height: '100%',
-        width: '100%',
-        minHeight: '100vh',
-        minWidth: '100vw',
-        display: 'flex',
-        flexDirection: 'column',
-        color: theme.colors.platinum
-
-    }
-}))
+  wrapper: {
+    height: '100%',
+    width: '100%',
+    minHeight: '100vh',
+    minWidth: '100vw',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    color: theme.colors.platinum
+  },
+  button: {
+    backgroundColor: theme.colors.lavender[0],
+    borderRadius: theme.radius.xl,
+    width: '165px',
+    height: '36px',
+    color: theme.colors.primaryColor[0],
+    border: '2px solid #eaeaea'
+  }
+}));
 
 const Dashboard = (props: Props) => {
-    const { classes } = useStyles();
+  const { classes } = useStyles();
 
   return (
-    <div className={classes.wrapper}>Defender DAO</div>
-  )
-}
+    <div className={classes.wrapper}>
+      <h1>Defender DAO</h1>
+      <Button
+        // variant="gradient"
+        // gradient={{ from: '#c0a6ff', to: '#82c6f4', deg: 145 }}
+        className={classes.button}
+      >
+        I'm a BUTTON!
+      </Button>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
